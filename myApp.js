9515ed78a017ec7ecabadba529dcfccfb5f50d6b
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-05-11 15:24:35 -0500

    First push

--- myApp.js
+++ myApp.js
@@ -1,11 +1,21 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-
-
-
-
-
+console.log("Hello World")
+ 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json",(req,res)=>{
+  const name1 = process.env.MESSAGE_STYLE
+if(name1 === "uppercase"){
+   res.json({"message":"Hello json".toUpperCase()})
+ }else{
+  res.json({"message":"Hello json"})
+ }
+})
 
 
 
