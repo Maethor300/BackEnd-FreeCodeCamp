3e8e53dfbd957c0fcb89921882ebcf3128b67113
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-05-13 13:56:02 -0500

    req.params, req.query & formularios

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
 
@@ -9,7 +10,7 @@ app.use((req, res, next) => {
 	console.log(string);
 	next();
 });
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.get("/json",(req,res)=>{
   const name1 = process.env.MESSAGE_STYLE
 if(name1 === "uppercase"){
@@ -30,11 +31,34 @@ app.get("/now", function(req,res,next){
   res.json({time:req.time})
 });
 
+app.get("/:word/echo",function(req,res){
+	let word = req.params.word
+  res.json({echo:word})
+})
 
 
 
+app.route("/name").get( function(req, res) {
+	const {first} = req.query;
+	const {last} = req.query;
+	 
+	const { first: firstName, last: lastName } = req.query;
+	 
+	res.json({
+	  name: `${firstName} ${lastName}`
+	});
+  }).post(function(req,res){
+	const {first} = req.body;
+	const {last} = req.body;
+	 
+	const { first: firstName, last: lastName } = req.body;
+	 
+	res.json({
+	  name: `${firstName} ${lastName}`
+	});
+  });
 
-
+  
 
 
 
